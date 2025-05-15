20670b0882595a12ee2882d977dd20805c5d2653
preek0719 <preetha.krishnan@softwire.com>
2025-05-15 10:12:15 +0100

    2nd commit

--- MyFace/src/UsersPage/UsersPage.tsx
+++ MyFace/src/UsersPage/UsersPage.tsx
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import "./UsersPage.scss";
 
- type usersData = {
+type usersData = {
     profileImageUrl: string;
     name: string;
- }
+}
 
-export  function UsersPage() {
+export function UsersPage() {
 
-     const [usersData, setUsersData] = useState<usersData[] | null>(null);
+    const [usersData, setUsersData] = useState<usersData[] | null>(null);
 
-      useEffect(() => {
-            fetch("http://localhost:3001/users")
+    useEffect(() => {
+        fetch("http://localhost:3001/users")
             .then(response => response.json())
             .then(response => setUsersData(response.results));
-         }, [usersData]);
-     
-         console.log(usersData);
-if (!usersData) {
+    }, [usersData]);
+
+    console.log(usersData);
+    if (!usersData) {
         return <div>Waiting for data!</div>
-    }   
+    }
     return (
-    <ul id='usersContainer'>       
-        {usersData.map((user: usersData, i: number)=>
-            <li id='userList' key={i}>
-                <p id='username'><b>{user.name}</b></p>
-                <img id='userProfileImage' src={user.profileImageUrl}></img>               
-            </li>)}
-        </ul>
+        <div>
+            <h2>Users</h2>
+            <ul id='usersContainer'>
+                {usersData.map((user: usersData, i: number) =>
+                    <li id='userList' key={i}>
+                        <p id='username'><b>{user.name}</b></p>
+                        <img id='userProfileImage' src={user.profileImageUrl}></img>
+                    </li>)}
+            </ul>
+        </div>
     );
 }
 
